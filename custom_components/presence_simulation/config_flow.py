1259a204e65c2331856411c28dc944b850044b9f
christophe <christophe.desirant@gmail.com>
2024-02-22 11:47:32 +0100

    Forbit change of the switch name

--- custom_components/presence_simulation/config_flow.py
+++ custom_components/presence_simulation/config_flow.py
@@ -1,4 +1,5 @@
 from homeassistant import config_entries
+import homeassistant.helpers.config_validation as cv
 import logging
 import voluptuous as vol
 from .const import DOMAIN
@@ -6,7 +7,6 @@ from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 class PresenceSimulationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
     VERSION = 2
     data = None
     async def async_create_flow(handler, context, data):
@@ -15,8 +15,8 @@ class PresenceSimulationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             """Finish flow."""
     async def async_step_user(self, info=None):
         data_schema = {
-            vol.Required("switch", default="Presence simulation"): str,
-            vol.Required("entities"): str,
+            vol.Required("switch", description={"suggested_value": "Choose a unique name"}): str,
+            vol.Required("entities"): str, #cv.entity_ids,
             vol.Required("delta", default=7): int,
             vol.Required("interval", default=30): int,
             vol.Required("restore", default=False): bool,
@@ -49,31 +49,46 @@ class PresenceSimulationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class OptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
         self.config_entry = config_entry
-    async def async_step_init(self, info=None):
-        """Manage the options."""
-        if not info:
-            if "interval" in self.config_entry.data:
-                interval = self.config_entry.data["interval"]
-            else:
-                interval = 30
-            if "restore" in self.config_entry.data:
-                restore = self.config_entry.data["restore"]
-            else:
-                restore = 0
-            if "random" in self.config_entry.data:
-                random = self.config_entry.data["random"]
-            else:
-                random = 0
 
-            data_schema = {
-                vol.Required("switch", default=self.config_entry.data["switch"]): str,
-                vol.Required("entities", default=self.config_entry.data["entities"]): str,
-                vol.Required("delta", default=self.config_entry.data["delta"]): int,
-                vol.Required("interval", default=interval): int,
-                vol.Required("restore", default=restore): bool,
-                vol.Required("random", default=random): int,
-            }
+    async def async_step_init(self, info=None):
+        _LOGGER.debug("config flow init %s", info)
+
+        if "interval" in self.config_entry.data:
+            interval = self.config_entry.data["interval"]
+        else:
+            interval = 30
+        if "restore" in self.config_entry.data:
+            restore = self.config_entry.data["restore"]
+        else:
+            restore = 0
+        if "random" in self.config_entry.data:
+            random = self.config_entry.data["random"]
+        else:
+            random = 0
+
+        data_schema = {
+            vol.Required("switch", default=self.config_entry.data["switch"]): str,
+            vol.Required("entities", default=self.config_entry.data["entities"]): str,
+            vol.Required("delta", default=self.config_entry.data["delta"]): int,
+            vol.Required("interval", default=interval): int,
+            vol.Required("restore", default=restore): bool,
+            vol.Required("random", default=random): int,
+        }
+        _LOGGER.debug("switch %s", self.config_entry.data["switch"])
+        _LOGGER.debug("config_entry data %s", self.config_entry.data)
+        _LOGGER.debug("will async_show_form")
+
+        if not info:
             return self.async_show_form(
                 step_id="init", data_schema=vol.Schema(data_schema)
             )
+
+        #if pop-up is saved but the name has changed, log an error and ask again
+        if info["switch"] != self.config_entry.data["switch"]:
+            _LOGGER.error("Presence Simulation Switch name can't be changed")
+            return self.async_show_form(
+                step_id="init", data_schema=vol.Schema(data_schema)
+            )
+
         return self.async_create_entry(title="Simulation Presence", data=info)
+        #return self.async_create_entry(title=self.config_entry.data["switch"], data=info)
