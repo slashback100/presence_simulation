aed735f66cd1093059ac5c0b23d09dc311834641
slashback100 <christophe.desirant@gmail.com>
2025-08-06 22:23:35 +0200

    fix #177

--- custom_components/presence_simulation/__init__.py
+++ custom_components/presence_simulation/__init__.py
@@ -472,7 +472,7 @@ async def async_setup_entry(hass, entry):
 
         else:
             _LOGGER.debug("Switching entity %s to %s", entity_id, state.state)
-            if state.state == "on" or state.state == "off" or (state.state == "unavailable_as_off" and unavailable_as_off):
+            if state.state == "on" or state.state == "off" or (state.state == "unavailable" and unavailable_as_off):
                 s = "on" if state.state == "on" else "off"
                 await hass.services.async_call("homeassistant", "turn_"+s, service_data, blocking=False, target={"entity_id": entity_id})
                 event_data = {"entity_id": entity_id, "service": "homeassistant.turn_"+s, "service_data": service_data}
